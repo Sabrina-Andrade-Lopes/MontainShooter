1955f212e5806e8f8724262734610b00ef1b2e73
Seu Nome <seuemail@exemplo.com>
2026-06-15 23:51:37 -0300

    commit2

--- code/game.py
+++ code/game.py
@@ -8,17 +8,14 @@ from code.menu import Menu
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(576, 324))
 
     def run(self):
+
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-            # Check for all events
-           # for event in pygame.event.get():
-             #   if event.type == pygame.QUIT:
-                #    pygame.quit() # Close Window
-               #     quit() # End Pygame
+
 
 
